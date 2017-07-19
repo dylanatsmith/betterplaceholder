e2b6be38c5dcfaeebe5b8520a07db1a38276e76f
Dylan Smith <dylanatsmith@gmail.com>
2017-07-19 20:33:42 +0100

    Initial commit

--- sections/_footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-	<div class="container">
-		<div class="row footer__row">
-			<div class="col-xs-12 footer__logo center-children">
-				<img src="/assets/img/logo-solo-frown.svg">
-			</div>
-		</div>
-	</div>
-</footer>
