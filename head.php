e4f051c1f5ba1f9ef00a113e2f5e729d7580aaa4
Dylan Smith <dylanatsmith@gmail.com>
2017-07-19 19:42:57 +0100

    Initial commit

---
+++ head.php
@@ -0,0 +1,37 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Solo — The Conference for Freelancers</title>
+    <meta name="description" content="Meta description">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/assets/img/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/assets/img/favicon/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/assets/img/favicon/manifest.json">
+    <link rel="mask-icon" href="/assets/img/favicon/safari-pinned-tab.svg" color="#f05a45">
+    <link rel="shortcut icon" href="/assets/img/favicon/favicon.ico">
+    <meta name="apple-mobile-web-app-title" content="Solo Conf">
+    <meta name="application-name" content="Solo Conf">
+    <meta name="msapplication-config" content="/img/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#f05a45">
+    <!-- Styles and scripts -->
+    <link rel="stylesheet" href="/css/main.css">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fontlibrary.org/face/overpass" type="text/css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Archivo+Black" type="text/css">
+    <!-- Open Graph -->
+    <meta property="og:url" content="https://solo-conf.com">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Solo — The Conference for Freelancers">
+    <meta property="og:image" content=""/>
+    <meta property="og:description" content="">
+    <meta property="og:site_name" content="Solo — The Conference for Freelancers">
+    <!-- Twitter card -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@soloconf">
+    <meta name="twitter:title" content="Solo — The Conference for Freelancers">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="">
+    <meta name="twitter:alt" content="">
+</head>
---
+++ sections/_footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+	<div class="container">
+		<div class="row footer__row">
+			<div class="col-xs-12 footer__logo center-children">
+				<img src="/assets/img/logo-solo-frown.svg">
+			</div>
+		</div>
+	</div>
+</footer>
