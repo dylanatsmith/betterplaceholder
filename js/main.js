ff50be025af011dab5ee5d57a5ce1a9ee459781f
Dylan Smith <dylanatsmith@gmail.com>
2017-07-20 08:40:00 +0100

    Changed field update events

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
+// Updates value for width every time a key is entered in that field
 $(function () {
   var $width      = $('#width');
   var $inputWidth = $('#input-width');
-  $inputWidth.on('keydown', function () {
+  $inputWidth.on('change keydown paste', function () {
     setTimeout(function () {
       $width.html($inputWidth.val());
     }, 0);
   });
 })
+
+// if width isn't empty 
+// 	 set height to 'x' + 'value in height input'
+// else
+// 	show warning that a width must be entered
+// 
+// Alternatively: Grey out height until width is entered?
+
+// 
