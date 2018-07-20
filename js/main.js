b6645dcabeefe084eb7a8892b4ed9da18fd357cc
Dylan Smith <dylanatsmith@gmail.com>
2018-07-20 21:11:01 +0100

    Removed extraneous function wrapping

--- js/main.js
+++ js/main.js
@@ -17,69 +17,55 @@ var $inputString     = $('#input-string');
 var $filetype        = $('#filetype');
 
 // Reenable preview and reset buttons when any input changes
-$(function () {
-  $( 'input' ).on( 'change keydown paste', function () {
-    $( '.button--preview, .button--reset, .button--copy' ).removeClass( 'button--disabled' );
-  });
-})
+$( 'input' ).on( 'change keydown paste', function () {
+  $( '.button--preview, .button--reset, .button--copy' ).removeClass( 'button--disabled' );
+});
 
 // Updates value for width every time a key is entered in that field
-$(function () {
-  $inputWidth.on( 'change keydown paste', function () {
-    setTimeout(function () {
-      $width.html($inputWidth.val());
-    }, 0);
-  });
-})
+$inputWidth.on( 'change keydown paste', function () {
+  setTimeout(function () {
+    $width.html($inputWidth.val());
+  }, 0);
+});
 
 // Updates value for height every time a key is entered in that field
-$(function () {
-  $inputHeight.on( 'change keydown paste', function () {
-    setTimeout(function () {
-      $height.html('x' + $inputHeight.val());
-    }, 0);
-  });
-})
+$inputHeight.on( 'change keydown paste', function () {
+  setTimeout(function () {
+    $height.html('x' + $inputHeight.val());
+  }, 0);
+});
 
 // Updates value for background colour every time a key is entered in that field
-$(function () {
-  $inputBgcolour.on('change keydown paste', function () {
-    setTimeout(function () {
-      $bgcolour.html( '/' + $inputBgcolour.val().replace( '#', '' ) );
-      $textcolour.html('/' + $inputTextcolour.val().replace( '#', '' ) );
-    }, 0);
-  });
-})
+$inputBgcolour.on('change keydown paste', function () {
+  setTimeout(function () {
+    $bgcolour.html( '/' + $inputBgcolour.val().replace( '#', '' ) );
+    $textcolour.html('/' + $inputTextcolour.val().replace( '#', '' ) );
+  }, 0);
+});
+
 
 // Updates value for text colour every time a key is entered in that field
-$(function () {
-  $inputTextcolour.on('change keydown paste', function () {
-    setTimeout(function () {
-      $bgcolour.html( '/' + $inputBgcolour.val().replace( '#', '' ) );
-      $textcolour.html('/' + $inputTextcolour.val().replace( '#', '' ) );
-    }, 0);
-  });
-})
+$inputTextcolour.on('change keydown paste', function () {
+  setTimeout(function () {
+    $bgcolour.html( '/' + $inputBgcolour.val().replace( '#', '' ) );
+    $textcolour.html('/' + $inputTextcolour.val().replace( '#', '' ) );
+  }, 0);
+});
+
 
 // Updates value for string every time a key is entered in that field
-$(function () {
-  $inputString.on('change keydown paste', function () {
-    setTimeout(function () {
-      // Change spaces for plus signs
-      var str = $inputString.val();
-      var newStr = '?text=' + str.replace( /\s+/g, '+' );
-      // Display string in URL
-      $string.html( newStr );
-    }, 0);
-  });
-})
+$inputString.on('change keydown paste', function () {
+  // Change spaces for plus signs
+  var str = $inputString.val();
+  var newStr = '?text=' + str.replace( /\s+/g, '+' );
+  // Display string in URL
+  $string.html( newStr );
+});
 
 // Updates value for filetype every time a new radio button is clicked
-$(function () {
-  $('input[name=filetype]').on('change', function () {
-      $filetype.html( '.' + $('input[name=filetype]:checked').val() );
-  });
-})
+$('input[name=filetype]').on('change', function () {
+    $filetype.html( '.' + $('input[name=filetype]:checked').val() );
+});
 
 // Instantiate clipboard.js
 var clipboard = new Clipboard('.button--copy', {
